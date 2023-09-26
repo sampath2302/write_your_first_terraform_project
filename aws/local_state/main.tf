e88a1c5b6cfffed416c9e4b9fb2ef4e35bf77e2d
sampath2302 <73831564+sampath2302@users.noreply.github.com>
2023-09-26 09:42:16 +0800

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -9,6 +9,10 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+provider "aws" {
+  region  = "us-east-1"
+}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
